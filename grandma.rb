4b6a656ea782addf55fe81ee12bdd5f8cefa2d32
Michael Piercy <michael.piercy@zoho.com>
2020-04-14 22:13:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,8 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA'
     return == 'I LOVE YOU TOO PUMPKIN!'
-  elseif phrase != phrase.upcase()
+end
+  if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
