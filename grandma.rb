372771895e96c1f1bbef96b4b7b1419744f1c744
Michael Piercy <michael.piercy@zoho.com>
2020-04-14 21:07:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,4 +14,4 @@
 def speak_to_grandma
   if speak_to_grandma == "I LOVE YOU TOO PUMPKIN!"
   else if speak_to_grandma == "HUH?! SPEAK UP, SONNY!"
-  else if speak_to_grandma == ""
+  else if speak_to_grandma == "HUH?! SPEAK UP, SONNY!"
